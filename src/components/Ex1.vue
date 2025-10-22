1f0021b7e40b4c4e95cae18ac6eb4b0e1945b7eb
rodney4761 <rodney.wang.2024@smu.edu.sg>
2025-10-22 08:21:25 +0000

    F

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,8 +1,28 @@
 <script>
-    export default { 
-        // Add Code Here to complete the task
-        // Note: DO NOT USE "eval()". In security, "eval" is considered "evil"!!!
+export default {
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      operators: ["+", "-", "*", "/", "%"],
+      selectedOp: ""
     }
+  },
+  computed: {
+    result() {
+      let total = 0
+      const { x, y, selectedOp } = this
+      switch (selectedOp) {
+        case "+": total = x + y; break
+        case "-": total = x - y; break
+        case "*": total = x * y; break
+        case "/": total = y !== 0 ? x / y : "Error"; break
+        case "%": total = y !== 0 ? x % y : "Error"; break
+      }
+      return total
+    }
+  }
+}
 </script>
 
 <template>
