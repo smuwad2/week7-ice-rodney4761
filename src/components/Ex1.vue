552b9da69cf66719e14519d60812e080e90cf7e6
rodney4761 <rodney.wang.2024@smu.edu.sg>
2025-10-22 08:57:09 +0000

    F

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -24,21 +24,17 @@ export default {
   }
 }
 </script>
-
 <template>
     <p>x <input v-model.number="x"></p>
     <select v-model="selectedOp">
-        <option v-for="op in operators" >{{ op }}</option>
+        <option v-for="op in operators" :key="op" :value="op">{{ op }}</option>
     </select>
     <p>y <input v-model.number="y"></p>
     
     <p>---------------------</p>
-
     <p>= {{result}}</p>
 </template>
-
 <style scoped>
     p, input { font-family: monospace; }
     p { white-space: pre; }
-
 </style>
