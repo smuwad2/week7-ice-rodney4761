86ca97ea4b793aa42f1b1a57749651b9ced4c3bd
rodney4761 <rodney.wang.2024@smu.edu.sg>
2025-10-22 08:22:42 +0000

    F

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,8 +1,44 @@
 <script>
+    import axios from 'axios';
     export default { 
 
        // add code here
+        data() {
+            return {
+                moods: ['Happy', 'Sad', 'Angry'],
+                subject: '',
+                entry: '',
+                selMood: '',
+                outputMsg: ''
+            }
+        },
 
+        computed: {
+            baseUrl() {
+                if (window.location.hostname=='localhost')
+                    return 'http://localhost:3000' 
+                else {
+                    const codespace_host = window.location.hostname.replace('5173', '3000')
+                    return https://${codespace_host};
+                }
+            }
+        },
+        methods: {
+            addPost() {
+                axios.get(${this.baseUrl}/addPost, {params: {
+                    subject: this.subject,
+                    entry: this.entry,
+                    mood: this.selMood,
+                }}).then(response=> {
+                    this.outputMsg = response.data.message;
+
+                }).catch(
+                    error=>{
+                        console.log(error);
+                    }
+                )
+            }
+        }
     }
 </script>
 
@@ -19,14 +55,18 @@
 
         Mood:
         <!-- TODO: Build a dropdown list here for selecting the mood -->
+        <select name="" id="" v-model="selMood">
+            <option v-for="x in moods">{{ x }}</option>
+        </select>
 
         <br>
 
         <br>
-        <button>Submit New Post</button>
+        <button @click="addPost">Submit New Post</button>
+        <br>
+        {{ outputMsg }}
 
         <hr> Click  <a><router-link to="/ViewPosts/">here</router-link></a>  to return to Main Page
        
     </div>
 </template>
-
