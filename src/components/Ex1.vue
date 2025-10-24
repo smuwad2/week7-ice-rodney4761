fb59203c4c5fd1384ec53049ec550d0d14a3106c
rodney4761 <rodney.wang.2024@smu.edu.sg>
2025-10-24 14:58:24 +0000

    f

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,40 +1,48 @@
 <script>
-export default {
-  data() {
-    return {
-      x: 0,
-      y: 0,
-      operators: ["+", "-", "*", "/", "%"],
-      selectedOp: ""
+    export default { 
+        // Add Code Here to complete the task
+        // Note: DO NOT USE "eval()". In security, "eval" is considered "evil"!!!
+        data() {
+            return {
+                x: 0,
+                y: 0,
+                selectedOp: "",
+                operators: ['+', '-', '*', '/', '%']
+            }
+        },
+        computed: {
+            result() {
+                switch (this.selectedOp) {
+                    case '+':
+                        return this.x + this.y;
+                    case '-':
+                        return this.x - this.y;
+                    case '*':
+                        return this.x * this.y;
+                    case '/':
+                        return this.x / this.y;
+                    case '%':
+                        return this.x % this.y;
+                }
+            }
+        }
     }
-  },
-  computed: {
-    result() {
-      let total = 0
-      const { x, y, selectedOp } = this
-      switch (selectedOp) {
-        case "+": total = x + y; break
-        case "-": total = x - y; break
-        case "*": total = x * y; break
-        case "/": total = y !== 0 ? x / y : "Error"; break
-        case "%": total = y !== 0 ? x % y : "Error"; break
-      }
-      return total
-    }
-  }
-}
 </script>
+
 <template>
     <p>x <input v-model.number="x"></p>
     <select v-model="selectedOp">
-        <option v-for="op in operators" :key="op" :value="op">{{ op }}</option>
+        <option v-for="op in operators" >{{ op }}</option>
     </select>
     <p>y <input v-model.number="y"></p>
     
     <p>---------------------</p>
+
     <p>= {{result}}</p>
 </template>
+
 <style scoped>
     p, input { font-family: monospace; }
     p { white-space: pre; }
+
 </style>
